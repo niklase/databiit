9473a452bae33cf61e3867d39e90933afd27cb84
Niklas Eldberger <niklas.eldberger@gmail.com>
2025-02-11 20:46:45 +0100

    Fix some test typos and add services-app

--- java-object-gen/src/test/java/com/zuunr/api/objectgen/JavaModelCodeGeneratorTest.java
+++ java-object-gen/src/test/java/com/zuunr/api/objectgen/JavaModelCodeGeneratorTest.java
@@ -11,15 +11,15 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 public class JavaModelCodeGeneratorTest {
     @Test
     void classNameTest1() {
-        JsonArray pathToSchema = JsonArray.of("paths", "admin/users", "post", "requestBody", "content", "application/json", "schema");
+        JsonArray pathToSchema = JsonArray.of("paths", "/admin/users", "post", "requestBody", "content", "application/json", "schema");
         JsonObject openApiDoc = JsonObject.EMPTY.put(pathToSchema, JsonObject.EMPTY.put("type", "object"));
         String name = JavaModelCodeGenerator.createJavaClassName(pathToSchema, openApiDoc, "com.example");
-        assertEquals("com.example.admin_users.post.RequestBody", name);
+        assertEquals("com.example._admin_users.post.RequestBody", name);
     }
 
     @Test
     void classNameTest2() {
-        JsonArray pathToSchema = JsonArray.of("paths", "admin/users", "post", "requestBody", "content", "application/json", "schema");
+        JsonArray pathToSchema = JsonArray.of("paths", "/admin/users", "post", "requestBody", "content", "application/json", "schema");
         JsonObject openApiDoc = JsonObject.EMPTY.put(pathToSchema, JsonObject.EMPTY.put("type", "string"));
         String name = JavaModelCodeGenerator.createJavaClassName(pathToSchema, openApiDoc, "com.example");
         assertEquals("String", name);
@@ -28,7 +28,7 @@ public class JavaModelCodeGeneratorTest {
 
     @Test
     void createJavaClasses1() {
-        JsonArray pathToSchema = JsonArray.of("paths", "admin/users", "post", "requestBody", "content", "application/json", "schema");
+        JsonArray pathToSchema = JsonArray.of("paths", "/admin/users", "post", "requestBody", "content", "application/json", "schema");
         JsonObject openApiDoc = JsonObject.EMPTY.put(pathToSchema, JsonObject.EMPTY.put("type", "string"));
         JsonObject javaClasses = JavaModelCodeGenerator.createJavaClasses(openApiDoc, "com.example.myapi");
         assertEquals(JsonObject.EMPTY, javaClasses);
@@ -36,7 +36,7 @@ public class JavaModelCodeGeneratorTest {
 
     @Test
     void createJavaClasses2() {
-        JsonArray pathToSchema = JsonArray.of("paths", "admin/users", "post", "requestBody", "content", "application/json", "schema");
+        JsonArray pathToSchema = JsonArray.of("paths", "/admin/users", "post", "requestBody", "content", "application/json", "schema");
         JsonObject openApiDoc = JsonObject.EMPTY.put(pathToSchema, JsonObject.EMPTY
                 .put("type", "object")
                 .put("properties", JsonObject.EMPTY
@@ -45,7 +45,7 @@ public class JavaModelCodeGeneratorTest {
         JsonObject javaClasses = JavaModelCodeGenerator.createJavaClasses(openApiDoc, "io.someco.restapi");
 
         String javaClass = """
-                package io.someco.restapi.admin_users.post;
+                package io.someco.restapi._admin_users.post;
                 
                 public class RequestBody {
                     public final String prop1;
@@ -55,12 +55,12 @@ public class JavaModelCodeGeneratorTest {
                     }
                 }""";
 
-        assertEquals(JsonObject.EMPTY.put("io.someco.restapi.admin_users.post.RequestBody", javaClass), javaClasses);
+        assertEquals(JsonObject.EMPTY.put("io.someco.restapi._admin_users.post.RequestBody", javaClass), javaClasses);
     }
 
     @Test
     void createJavaClasses3() {
-        JsonArray pathToSchema = JsonArray.of("paths", "admin/users", "post", "requestBody", "content", "application/json", "schema");
+        JsonArray pathToSchema = JsonArray.of("paths", "/admin/users", "post", "requestBody", "content", "application/json", "schema");
         JsonObject openApiDoc = JsonObject.EMPTY.put(pathToSchema, JsonObject.EMPTY
                 .put("type", "object")
                 .put("properties", JsonObject.EMPTY
@@ -70,18 +70,18 @@ public class JavaModelCodeGeneratorTest {
         JsonObject javaClasses = JavaModelCodeGenerator.createJavaClasses(openApiDoc, "com.example");
 
         String javaClass1 = """
-                package com.example.admin_users.post;
+                package com.example._admin_users.post;
                 
                 public class RequestBody {
-                    public final com.example.admin_users.post.RequestBody_prop1 prop1;
+                    public final com.example._admin_users.post.RequestBody_prop1 prop1;
                 
-                    public final com.example.admin_users.post.RequestBody_prop1 getProp1() {
+                    public final com.example._admin_users.post.RequestBody_prop1 getProp1() {
                         return prop1;
                     }
                 }""";
 
         String javaClass2 = """
-                package com.example.admin_users.post;
+                package com.example._admin_users.post;
                 
                 public class RequestBody_prop1 {
                     public final String prop2;
@@ -92,13 +92,13 @@ public class JavaModelCodeGeneratorTest {
                 }""";
 
         assertEquals(JsonObject.EMPTY
-                        .put("com.example.admin_users.post.RequestBody", javaClass1)
-                        .put("com.example.admin_users.post.RequestBody_prop1", javaClass2),
+                        .put("com.example._admin_users.post.RequestBody", javaClass1)
+                        .put("com.example._admin_users.post.RequestBody_prop1", javaClass2),
                 javaClasses);
     }
     @Test
     void createJavaClasses4() {
-        JsonArray pathToSchema = JsonArray.of("paths", "admin/users", "post", "requestBody", "content", "application/json", "schema");
+        JsonArray pathToSchema = JsonArray.of("paths", "/admin/users", "post", "requestBody", "content", "application/json", "schema");
         JsonObject openApiDoc = JsonObject.EMPTY
                 .put(pathToSchema, JsonObject.EMPTY
                         .put("type", "object")
@@ -116,8 +116,8 @@ public class JavaModelCodeGeneratorTest {
 
         String expectedJson = """
                 {
-                  "com.example.admin_users.post.RequestBody_friends_items": "package com.example.admin_users.post;\\n\\npublic class RequestBody_friends_items {\\n    public final String firstName;\\n\\n    public final String getFirstName() {\\n        return firstName;\\n    }\\n}",
-                  "com.example.admin_users.post.RequestBody": "package com.example.admin_users.post;\\n\\npublic class RequestBody {\\n    public final List<com.example.admin_users.post.RequestBody_friends_items> friends;\\n\\n    public final List<com.example.admin_users.post.RequestBody_friends_items> getFriends() {\\n        return friends;\\n    }\\n}"
+                  "com.example._admin_users.post.RequestBody_friends_items": "package com.example._admin_users.post;\\n\\npublic class RequestBody_friends_items {\\n    public final String firstName;\\n\\n    public final String getFirstName() {\\n        return firstName;\\n    }\\n}",
+                  "com.example._admin_users.post.RequestBody": "package com.example._admin_users.post;\\n\\npublic class RequestBody {\\n    public final List<com.example._admin_users.post.RequestBody_friends_items> friends;\\n\\n    public final List<com.example._admin_users.post.RequestBody_friends_items> getFriends() {\\n        return friends;\\n    }\\n}"
                 }
                 """;
 
